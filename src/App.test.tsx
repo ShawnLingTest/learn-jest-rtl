eaac8dec8b127ace761b36240deaca3c9f570191
Ling Wang <2505660478@qq.com>
2023-02-25 20:56:30 +0800

    init

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test("test", () => {
-  expect(1 + 1).toBe(2);
+describe("test", () => {
+  test("first unit test", () => {
+    render(<App />);
+    expect(screen.getByText("Vite + React")).toBeInTheDocument();
+  });
 });
